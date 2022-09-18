c168d48643b5b56ec0c82e9914fa07c9d3cd61b3
Visualised <visually@protonmail.com>
2022-09-18 22:55:01 +0200

    Implement changes from the review process by Sebastian Ubli.

--- program.py
+++ program.py
@@ -1,55 +1,59 @@
+def print_menu():
+    menu = """0 - help (wyświetla się ta pomoc)
+1 - wylistowanie wszystkich nazw mieczy
+2 - pokazanie szczegółów danego miecza wg indeksu
+3 - wyłącz program"""
+    print(menu)
+
 def print_sword_list(sword_list):
-    for index, item in enumerate(sword_list):
-        print(f'{index} - {item["name"]}')
+    for index, sword in enumerate(sword_list):
+        print(f'{index} - {sword["name"]}')
 
 def print_sword_details(sword_list, sword_index):
-    print(f'{sword_list[sword_index]["name"]}, ' 
-        + f'Obrażenia: {sword_list[sword_index]["damage"]}, '
-        + f'Wymagana siła: {sword_list[sword_index]["required_strength"]}')
+    sword = sword_list[sword_index]
+    print(f'{sword["name"]}, ' 
+        + f'Obrażenia: {sword["damage"]}, '
+        + f'Wymagana siła: {sword["required_strength"]}')
 
 
 sword_list = [
     {
     "name": "Zardzewiały miecz", 
     "damage": 10, 
-    "required_strength": 5
+    "required_strength": 5,
     },
-
     {
     "name": "Miecz Świstaka", 
     "damage": 20, 
-    "required_strength": 15
+    "required_strength": 15,
     },
-
     {
     "name": "Miecz strachu", 
     "damage": 42, 
-    "required_strength": 18
+    "required_strength": 18,
     },
-
     {
     "name": "Miecz śmierci", 
     "damage": 48, 
-    "required_strength": 21
+    "required_strength": 21,
     },
-
     {
     "name": "Miecz Blizny", 
     "damage": 85, 
-    "required_strength": 70
+    "required_strength": 70,
     }
 ]
 
-menu = """0 - help (wyświetla się ta pomoc)
-1 - wylistowanie wszystkich nazw mieczy
-2 - pokazanie szczegółów danego miecza wg indeksu
-3 - wyłącz program"""
-
-print(menu)
+print_menu()
 while True:
-    user_selection = int(input("Wybierz opcje: "))
+    try:
+        user_selection = int(input("Wybierz opcje: "))
+    except ValueError:
+        print("Niepoprawna opcja, wybierz opcję ponownie.")
+        continue
+
     if user_selection == 0:
-        print(menu)
+        print_menu()
     elif user_selection == 1:
         print_sword_list(sword_list)
     elif user_selection == 2:
@@ -57,5 +61,3 @@ while True:
         print_sword_details(sword_list, sword_index)
     elif user_selection == 3:
         break
-    else:
-        print("Niepoprawna opcja, wybierz opcję ponownie.")
